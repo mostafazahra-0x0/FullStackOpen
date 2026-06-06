df4e1c2088f5f9a85f8c45c156f20783be365103
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-06-06 20:58:28 +0300

    Refactor App component structure by extracting Header, Part, Content, and Total components for better readability and maintainability.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,60 +1,36 @@
-const App = () => { 
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part, exercises }) => <div>{part} {exercises}</div>
+
+const Content = ({ parts }) => (
+  <div>
+    <Part part={parts[0].name} exercises={parts[0].exercises} />
+    <Part part={parts[1].name} exercises={parts[1].exercises} />
+    <Part part={parts[2].name} exercises={parts[2].exercises} />
+  </div>
+)
+
+const Total = ({ parts }) => (
+  <p>total is {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+)
+
+const App = () => {
   const course = {
     name: 'Half Stack application development',
     parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
     ]
-  } 
-  
-    return (
-      <>
-  
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
       <Total parts={course.parts} />
-  
-    </>
-    )
-  }
-  
-  export default App
-  
-  
-  
-  // 1.1
-  const Header = (props) => {
-    return (
-    <h1>{props.course}</h1>
-    )
-  }
-  const Part = (props) => {
-    return (
-    <div>{props.part} {props.exercises}</div>
-    )
-  }
-  const Content = (props) => {
-    return (
-      <div>
-        <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-        <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-        <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-      </div>
-    )
-  }
-  const Total = (props) => {
-    return (
-      <p>total is {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>  
-    )
-  }
-  
+    </div>
+  )
+}
+
+export default App
