db0a23dd7fe370e72eb857bde8c68d984729d11c
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-06-13 19:03:44 +0300

    Refactor learning-1 directory by removing unnecessary files and configurations, streamlining the project structure.

---
+++ part2/learning-1/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import Note from './components/Note'
+import { useState } from 'react'
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
+  return (
+    <div className='notes'>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input />
+        <button type="submit">save</button>
+      </form>   
+    </div>
+  )
+}
+
+export default App
