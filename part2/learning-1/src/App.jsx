408503e211e04d5ead4d50146bf6a2844da3e349
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-06-15 18:05:12 +0300

    feat: enhance note management with toggle functionality and add new notes to db.json

--- part2/learning-1/src/App.jsx
+++ part2/learning-1/src/App.jsx
@@ -28,12 +28,27 @@ const App = () => {
     event.preventDefault()
     console.log('button clicked', event.target)
     const noteObject = {
+        id: String(notes.length + 1),
         content: newNote,
         important: Math.random() < 0.5,
-        id: String(notes.length + 1),
       }
+      axios
+        .post('http://localhost:3001/notes', noteObject)
+        .then(response => {
+          setNotes(notes.concat(response.data))
+          setNewNote('')
+        })
       setNotes(notes.concat(noteObject))
       setNewNote('')
+  }
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id === id ? response.data : note))
+    })
   }
   return (
     <div className='notes'>
@@ -45,7 +60,12 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id} 
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)}
+            />
+
         )}
       </ul>
       <form onSubmit={addNote}>
