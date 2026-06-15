185c4b9486c239442fe9d4a81398fa36c394094c
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-06-15 14:12:50 +0300

    feat: integrate axios and json-server for note management in App component

--- part2/learning-1/src/App.jsx
+++ part2/learning-1/src/App.jsx
@@ -1,10 +1,22 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 import Note from './components/Note'
-import { useState } from 'react'
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState("")
   const [showAll, setShowAll] = useState(true)
+    const getHook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+    }
+    useEffect(getHook, [])
+    console.log('render', notes.length, 'notes')
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
