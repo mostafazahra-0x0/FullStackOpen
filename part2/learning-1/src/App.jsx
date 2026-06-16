683d1477cc1d9db0d8bd377b5c5f79a2a4f921db
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-06-16 14:27:55 +0300

    feat: update note importance and enhance UI with new components for notifications and footer

--- part2/learning-1/src/App.jsx
+++ part2/learning-1/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
+import noteService from './services/notes'
 import Note from './components/Note'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState("")
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
     const getHook = () => {
     console.log('effect')
     axios
@@ -38,21 +42,28 @@ const App = () => {
           setNotes(notes.concat(response.data))
           setNewNote('')
         })
-      setNotes(notes.concat(noteObject))
-      setNewNote('')
   }
   const toggleImportanceOf = id => {
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
-  
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(note => note.id === id ? response.data : note))
-    })
-  }
+    noteService
+      .update(id, changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(() => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      }
+    )}
   return (
     <div className='notes'>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      <Footer />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -65,7 +76,6 @@ const App = () => {
             note={note} 
             toggleImportance={() => toggleImportanceOf(note.id)}
             />
-
         )}
       </ul>
       <form onSubmit={addNote}>
