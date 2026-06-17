abdd192e72ad0e8bdda4cb2f6545037bc719e20e
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-06-17 23:01:02 +0300

    feat: prevent duplicate names in phonebook (exercise 2.18)

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react"
 import axios from 'axios'
-
-
+import { useEffect } from "react"
 const App = () => {
   const [search, setSearch] = useState('')
   const [countries, setCountries] = useState([])
@@ -11,8 +10,40 @@ const App = () => {
       .then(response => {
         setCountries(response.data)})
   }
+  useEffect(getCountries, [])
+  const handleSearchUpdate = (event) => {
+  setSearch(event.target.value)
+  }
+  const countriesToShow = countries.filter(c => 
+    c.name.common.toLowerCase().includes(search.toLowerCase())
+  )
+  const Country = countriesToShow[0]
   return (
     <div>
-    </div>
+      <input name="search" onChange={handleSearchUpdate} value={search}></input>
+      {countriesToShow.length > 10 && (
+        <p>Too many matches, specify another filter</p>
+      )}
+      {countriesToShow.length <= 10 && countriesToShow.length > 1 && (
+        countriesToShow.map(c =>
+          <p key={c.cca3}>{c.name.common}</p>
+        )
+      )} 
+      {countriesToShow.length === 1 && (
+        <div>
+          <h1>{countriesToShow[0].name.common}</h1>
+          <p>capital {countriesToShow[0].capital?.[0]}</p>
+          <p>area {countriesToShow[0].area}</p>
+          <h3>languages:</h3>
+          <ul>
+            {Object.values(countriesToShow[0].languages).map(language => (
+            <li key={language}>{language}</li>
+            ))}
+          </ul>
+          <img src={countriesToShow[0].flags.png} alt="flag" width="150" />
+        </div>
+      )}
+</div>
   )
 }
+export default App
