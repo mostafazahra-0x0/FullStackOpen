82e9eabd709fe71bb836f07cad0db06a2a886ab4
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-06-24 17:51:05 +0300

    resolve exercise 3.4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -44,6 +44,11 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(404).end()
   }
 })
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id != id)
+  response.status(204).end()
+})
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
