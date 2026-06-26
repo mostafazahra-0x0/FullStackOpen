3056bfcc7c7976f084413e6dd868a1f7a6ff6f73
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-06-26 17:20:39 +0300

    resolve exercise 3.10

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -8,6 +8,7 @@ morgan.token('body', (request, response) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 const cors = require('cors')
 app.use(cors())
+app.use(express.static('dist'))
 let persons = [
     {
       "id": "1",
