9fee6cb9d63909ea2dbc6a36ad6288304efabaad
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-06-29 14:13:37 +0300

    Connect notes backend to MongoDB Atlas

--- part3/learning-1/index.js
+++ part3/learning-1/index.js
@@ -1,64 +1,38 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-app.use(cors())
-app.use(express.json())
-app.use(express.static('dist'))
-
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World</h1>')
-})
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
-app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note=> note.id === id)
-  if (note) {
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+app.use(express.static("dist"));
+const Note = require("./models/note");
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World</h1>");
+});
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
+app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
-})
-const generateld = () => {
-  const maxId = notes.length > 0
-  ? Math.max(...notes.map(n => Number(n.id)))
-  : 0 
-  return String(maxId + 1)
-}
+  })
+});
 app.post('/api/notes', (request, response) => {
   const body = request.body
   if (!body.content) {
-      return response.status(400).json({
-        error:'contest missing'
-      })
-    }
-  const note = {
-      content: body.content,
-      important: body.important || false,
-      id: generateld(),
-    }
-    notes = notes.concat(note)
-    response.json(note)
+    return response.status(400).json({ error: 'content missing' })
+  }
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
