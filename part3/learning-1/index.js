ee218bd8835e6bf3dc9ac394d97a3847fc97e490
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-06-29 20:54:27 +0300

    delete, post, put in mongo db

--- part3/learning-1/index.js
+++ part3/learning-1/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
+app.use(requestLogger)
 const Note = require("./models/note");
 app.get("/", (request, response) => {
   response.send("<h1>Hello World</h1>");
@@ -14,10 +15,16 @@ app.get("/api/notes", (request, response) => {
     response.json(notes);
   });
 });
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 });
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -31,6 +38,40 @@ app.post('/api/notes', (request, response) => {
   note.save().then(savedNote => {
     response.json(savedNote)
   })
+})
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  next(error)
+}
+app.use(errorHandler)
+app.delete('/api/notes/:id', (request, response, next) => {
+ Note.findByIdAndDelete(request.params.id)
+  .then(() => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
+app.put('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+      note.content = content
+      note.important = important
+      return note.save()
+        .then((updatedNote) => {
+          response.json(updatedNote)
+        })
+    })
+    .catch(error => next(error))
 })
 const PORT = process.env.PORT
 app.listen(PORT, () => {
