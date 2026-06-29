3e858f316b862fc4b8776481df07a13b58e85989
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-06-29 21:21:26 +0300

    completed exercise 3.16

--- part3/learning-1/index.js
+++ part3/learning-1/index.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
-app.use(requestLogger)
 const Note = require("./models/note");
 app.get("/", (request, response) => {
   response.send("<h1>Hello World</h1>");
@@ -26,7 +25,7 @@ app.get("/api/notes/:id", (request, response, next) => {
     })
     .catch(error => next(error))
 });
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
   if (!body.content) {
     return response.status(400).json({ error: 'content missing' })
@@ -35,9 +34,35 @@ app.post('/api/notes', (request, response) => {
     content: body.content,
     important: body.important || false,
   })
-  note.save().then(savedNote => {
-    response.json(savedNote)
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
+})
+app.delete('/api/notes/:id', (request, response, next) => {
+ Note.findByIdAndDelete(request.params.id)
+  .then(() => {
+    response.status(204).end()
   })
+  .catch(error => next(error))
+})
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      if (updatedNote) {
+        response.json(updatedNote)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -51,28 +76,6 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 app.use(errorHandler)
-app.delete('/api/notes/:id', (request, response, next) => {
- Note.findByIdAndDelete(request.params.id)
-  .then(() => {
-    response.status(204).end()
-  })
-  .catch(error => next(error))
-})
-app.put('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (!note) {
-        return response.status(404).end()
-      }
-      note.content = content
-      note.important = important
-      return note.save()
-        .then((updatedNote) => {
-          response.json(updatedNote)
-        })
-    })
-    .catch(error => next(error))
-})
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -17,39 +17,43 @@ const personSchema = new mongoose.Schema({
   number: String,
 })
 const Person = mongoose.model('Person', personSchema)
-app.get('/', (request, response) => {
-response.send('<h1>hi</h1>')
+app.get('/', (request, response, next) => {
+  response.send('<h1>hi</h1>')
 })
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(persons => {
     response.json(persons)
   })
+  .catch(error => next(error))
 })
-app.get('/info', (request, response) => {
-  response.send(
+app.get('/info', (request, response, next) => {
+  Person.find({}).then(persons => {
+    response.send(
     `<h3>we have ${persons.length} people</h3>` +
     `<p>${new Date()}</p>`
     )
+  })
+  .catch(error => next(error))
 })
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id ==id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error)) 
 })
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(() => {
       response.status(204).end()
     })
-    .catch(error => {
-      response.status(404).end()
+    .catch(error => next(error))
     })
-})
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
   if (!body.name || !body.number) {
     return response.status(400).json({ error: 'content missing' })
@@ -61,7 +65,16 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  next(error)
+}
+app.use(errorHandler)
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
