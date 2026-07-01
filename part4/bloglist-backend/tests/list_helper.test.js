2111332c67480a9fdfb8fd06aebede7717a90a11
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-07-01 23:34:40 +0300

    completed exercise 4.4

--- part4/bloglist-backend/tests/list_helper.test.js
+++ part4/bloglist-backend/tests/list_helper.test.js
@@ -7,3 +7,21 @@ describe('dummy', () => {
     assert.strictEqual(listHelper.dummy([]), 1)
   })
 })
+describe('total likes', () => {
+    test('total likes', () => {
+      const result = listHelper.totalLikes([
+        { likes: 5 },
+        { likes: 3 },
+        { likes: 2 },
+      ])
+      assert.strictEqual(result, 10)
+    })
+    test('of empty list is zero', () => {
+      const result = listHelper.totalLikes([])
+      assert.strictEqual(result, 0)
+    })
+    test('when list has only one blog, equals the likes of that', () => {
+      const result = listHelper.totalLikes([{ likes: 5 }])
+      assert.strictEqual(result, 5)
+    })    
+})
