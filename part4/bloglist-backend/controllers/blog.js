581f838b974b305faef970831b11d7416b46705e
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-07-01 18:24:40 +0300

    completed exercise 3.24

---
+++ part4/bloglist-backend/controllers/blog.js
@@ -0,0 +1,58 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogRouter.get('/', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs)
+  })
+})
+
+blogRouter.get('/:id', (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then(blog => {
+      if (blog) {
+        response.json(blog)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+blogRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body) 
+
+  blog.save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+blogRouter.delete('/:id', (request, response, next) => {
+  Blog.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+blogRouter.put('/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Blog.findById(request.params.id)
+    .then(blog => {
+      if (!blog) {
+        return response.status(404).end()
+      }
+
+      blog.content = content
+      blog.important = important
+
+      return blog.save().then((updatedBlog) => {
+        response.json(updatedBlog)
+      })
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogRouter
