102742a2e6d595cc0938b89902a99bf34e38db2e
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-07-03 14:34:55 +0300

    completed exercise 4.8

--- part4/bloglist-backend/controllers/blog.js
+++ part4/bloglist-backend/controllers/blog.js
@@ -7,16 +7,9 @@ blogRouter.get('/', (request, response) => {
   })
 })
 
-blogRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(blog => {
-      if (blog) {
-        response.json(blog)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
 blogRouter.post('/', (request, response) => {
