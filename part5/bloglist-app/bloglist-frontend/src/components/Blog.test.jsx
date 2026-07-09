6ad588013436b4d4ce73433c53da6cc1b9e19c3f
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-07-09 09:26:42 +0300

    completed exercise 5.14

--- part5/bloglist-app/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-app/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,19 +1,29 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+const blog = {
+  title: 'Component testing is done with react-testing-library',
+  author: 'Kent C. Dodds',
+  url: 'https://example.com/blog',
+  likes: 5,
+  user: { username: 'kentcdodds' }
+}
 
 test('renders title and author, but not url or likes by default', () => {
-  const blog = {
-    title: '...',
-    author: '...',
-    url: '...',
-    likes: 5,
-    user: { username: '...' }
-  }
-
   render(<Blog blog={blog} />)
 
   expect(screen.getByText(blog.title, { exact: false })).toBeDefined()
   expect(screen.getByText(blog.author, { exact: false })).toBeDefined()
   expect(screen.queryByText(blog.url)).toBeNull()
   expect(screen.queryByText(blog.likes)).toBeNull()
+})
+test('renders url and likes when expanded', async () => {
+  render(<Blog blog={blog} />)
+  const user = userEvent.setup()
+
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  expect(screen.getByText(blog.url, { exact: false })).toBeDefined()
+  expect(screen.getByText(blog.likes, { exact: false })).toBeDefined()
 })
