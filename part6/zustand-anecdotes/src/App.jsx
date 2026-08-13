a9aef806d14c5b4ce32f88f3d3a84e720c110c98
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-08-13 07:22:47 +0300

    implement voting and adding new anecdotes (6.2, 6.3)

---
+++ part6/zustand-anecdotes/src/App.jsx
@@ -0,0 +1,34 @@
+import { useAnecdotes, useAnecdoteActions } from './store'
+
+const App = () => {
+  const anecdotes = useAnecdotes()
+  const { vote, add } = useAnecdoteActions()
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    add(content)
+    event.target.reset()
+  }
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+export default App
