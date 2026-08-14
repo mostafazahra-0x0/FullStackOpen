3836d744ead15b7079a46df611d1267b0b911371
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-08-14 14:51:56 +0300

    completed exercise 6.9

--- part6/zustand-anecdotes/src/services/anecdotes.js
+++ part6/zustand-anecdotes/src/services/anecdotes.js
@@ -2,11 +2,9 @@ const baseUrl = 'http://localhost:3001/anecdotes'
 
 const getAll = async () => {
   const response = await fetch(baseUrl)
-
   if (!response.ok) {
     throw new Error('Failed to fetch anecdotes')
   }
-
   return await response.json()
 }
 
@@ -16,12 +14,22 @@ const createNew = async (content) => {
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ content, votes: 0 }),
   })
-
   if (!response.ok) {
     throw new Error('Failed to create anecdote')
   }
-
   return await response.json()
 }
 
-export default { getAll, createNew }
+const update = async (id, anecdote) => {
+  const response = await fetch(`${baseUrl}/${id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(anecdote),
+  })
+  if (!response.ok) {
+    throw new Error('Failed to update anecdote')
+  }
+  return await response.json()
+}
+
+export default { getAll, createNew, update }
--- part6/zustand-anecdotes/src/store.js
+++ part6/zustand-anecdotes/src/store.js
@@ -1,15 +1,19 @@
 import { create } from 'zustand'
 import anecdoteService from './services/anecdotes'
 
-const useAnecdoteStore = create((set) => ({
+const useAnecdoteStore = create((set, get) => ({
   anecdotes: [],
   filter: '',
   actions: {
-    vote: (id) => set((state) => ({
-      anecdotes: state.anecdotes.map((anecdote) =>
-        anecdote.id === id ? { ...anecdote, votes: anecdote.votes + 1 } : anecdote
-      ),
-    })),
+    vote: async (id) => {
+      const anecdote = get().anecdotes.find(a => a.id === id)
+      const updated = await anecdoteService.update(
+        id, { ...anecdote, votes: anecdote.votes + 1 }
+      )
+      set((state) => ({
+        anecdotes: state.anecdotes.map(a => a.id === id ? updated : a)
+      }))
+    },
     add: async (content) => {
       const newAnecdote = await anecdoteService.createNew(content)
       set((state) => ({
