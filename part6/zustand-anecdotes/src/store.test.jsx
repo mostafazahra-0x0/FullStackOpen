f84be8ed11da04ec66cc8734f1df2257ebc866a9
mostafazhara0x0 <mostafazahra.root.0x0@gmail.com>
2026-08-19 18:40:12 +0300

    complete exercise 6.13

--- part6/zustand-anecdotes/src/store.test.jsx
+++ part6/zustand-anecdotes/src/store.test.jsx
@@ -1,6 +1,5 @@
-import { describe, it, expect, beforeEach, vi } from 'vitest'
-import { renderHook, act, render } from '@testing-library/react'
-
+import { describe, it, expect, beforeEach, vi, afterEach } from 'vitest'
+import { renderHook, act, render, cleanup } from '@testing-library/react'
 vi.mock('./services/anecdotes', () => ({
   default: {
     getAll: vi.fn(),
@@ -53,10 +52,31 @@ describe('useAnecdoteStore', () => {
     const a = getByText('A')
     const c = getByText('C')
     expect(
-      b.compareDocumentPosition(a) & Node.DOCUMENT_POSITION_FOLLOWING
+      b.compareDocumentPosition(a)
+      & Node.DOCUMENT_POSITION_FOLLOWING
     ).toBeTruthy()
     expect(
-      a.compareDocumentPosition(c) & Node.DOCUMENT_POSITION_FOLLOWING
+      a.compareDocumentPosition(c)
+      & Node.DOCUMENT_POSITION_FOLLOWING
     ).toBeTruthy()
   })
+  it('renders only anecdotes matching the filter', () => {
+    const anecdotes = [
+      { content: 'JavaScript is soo hard', id: 1, votes: 2 },
+      { content: 'React is great', id: 2, votes: 5 },
+      { content: 'I do not like testing', id: 3, votes: 1 }
+    ]
+    useAnecdoteStore.setState({
+      anecdotes,
+      filter: 'no'
+    })
+    const { getAllByText, queryByText } = render(<AnecdoteList />)
+    expect(getAllByText('I do not like testing')).toHaveLength(1)
+    expect(queryByText('React is great')).toBeNull()
+    expect(queryByText('JavaScript is soo hard')).toBeNull()
+  })
+  
+})
+afterEach(() => {
+  cleanup()
 })
